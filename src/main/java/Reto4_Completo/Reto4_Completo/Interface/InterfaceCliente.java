5ad8b158bf07c4bcda948af48cc4b2ca0408256c
unknown <humberto.barbosa.mt@correo.usa.edu.co>
2021-11-01 17:42:03 -0500

    Proyecto Reto3 listo para subir a la instancia

---
+++ src/main/java/Reto4_Completo/Reto4_Completo/Interface/InterfaceCliente.java
@@ -0,0 +1,8 @@
+package Reto4_Completo.Reto4_Completo.Interface;
+
+import Reto4_Completo.Reto4_Completo.Modelo.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InterfaceCliente extends CrudRepository<Cliente,Integer> {
+
+}
